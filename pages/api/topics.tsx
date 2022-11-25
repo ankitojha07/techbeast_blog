61adcbb738b66a4b1f1b58ca957a56879310c5da
Ankit Ojha <ankitojha1409@gmail.com>
2022-11-25 10:31:46 +0530

    dynamic routing started

---
+++ pages/api/topics.tsx
@@ -0,0 +1,18 @@
+export default [
+    {
+        id:'angular',
+        about:'Angular id a development platform, built on Typescript.'
+    },
+    {
+        id:'vue',
+        about:'vue is a development library platform, built on Typescript.'
+    },
+    {
+        id:'react',
+        about:'React js is a Library , built on Typescript.'
+    },
+    {
+        id:'next',
+        about:'Next js is a development platform, built on Typescript.'
+    }
+]

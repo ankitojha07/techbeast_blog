2b53cd6d7e7123948a8a8f167e53d19f8841e0ff
Ankit Ojha <ankitojha1409@gmail.com>
2022-11-25 10:17:16 +0530

    custom pages added

---
+++ pages/hello.tsx
@@ -0,0 +1,10 @@
+export default function Hello(){
+    return (
+        <>
+        <h1>Learn Next JS.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo. Saepe fuga rerum laudantium tempora minima sapiente minus nemo voluptatum nihil molestias commodi temporibus blanditiis, fugit libero? Doloremque molestias ea voluptatibus maxime ipsam corporis illum, neque, modi cumque ipsum reiciendis asperiores distinctio dignissimos, perferendis eaque ut sed magni deserunt id.</p>
+        <h2>Content system.</h2>
+        <button>Click me</button>
+        </>
+     )
+}
---
+++ pages/learn/next.tsx
@@ -0,0 +1,9 @@
+export default function LearnNext(){
+    return (
+        <>
+        <div>
+            <h1>This file is for learning Next JS</h1>
+        </div>
+        </>
+    )
+}

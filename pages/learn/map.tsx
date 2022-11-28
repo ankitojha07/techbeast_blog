b1f2b220a56176e223167267f59017c6af2f95ad
Ankit Ojha <ankitojha1409@gmail.com>
2022-11-29 00:40:31 +0530

    Hooks learning : added useState

---
+++ pages/learn/map.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import React from 'react'
+
+const map = () => {
+    const initialCount =0
+    const [count,setCount] = useState(initialCount)
+  return (
+    <div>
+        Count : { count}
+        <button onClick={()=>{setCount(initialCount)}}>Reset</button>
+        <button onClick={()=>{setCount(prevCount => prevCount+1)}}>Increase</button>
+        <button onClick={()=>{setCount(prevCount => prevCount-1)}}>Decrrease</button>
+    </div>
+  )
+}
+
+export default map

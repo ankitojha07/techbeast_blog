654955242fc51b2850c86c7d0218df6e0c77424c
Ankit Ojha <ankitojha1409@gmail.com>
2022-11-30 11:11:02 +0530

    added objects for book details

---
+++ data/books.jsx
@@ -0,0 +1,27 @@
+const books = [
+  {
+    id: "1",
+    name: "Mindset",
+    description: "This is my first book.",
+  },
+  {
+    id: "2",
+    name: "How to get rich",
+    description: "This is my 2nd book.",
+  },
+  {
+    id: "3",
+    name: "How to sell drugs online and fast.",
+    description: "This is my 3rd book.",
+  },
+  {
+    id: "4",
+    name: "Crack the hole.",
+    description: "This is my 4th book.",
+  },
+  {
+    id: "5",
+    name: "Rch dad poor dad",
+    description: "This is my 5th book.",
+  },
+];

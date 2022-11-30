0efadb8548eda46c1584f0535b627c212982540b
Ankit Ojha <ankitojha1409@gmail.com>
2022-12-01 02:11:45 +0530

    para tag added to home

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -3,6 +3,9 @@ export default function Hello(){
         <>
         <h1>Learn Next JS.</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo. Saepe fuga rerum laudantium tempora minima sapiente minus nemo voluptatum nihil molestias commodi temporibus blanditiis, fugit libero? Doloremque molestias ea voluptatibus maxime ipsam corporis illum, neque, modi cumque ipsum reiciendis asperiores distinctio dignissimos, perferendis eaque ut sed magni deserunt id.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo. Saepe fuga rerum laudantium tempora minima sapiente minus nemo voluptatum nihil molestias commodi temporibus blanditiis, fugit libero? Doloremque molestias ea voluptatibus maxime ipsam corporis illum, neque, modi cumque ipsum reiciendis asperiores distinctio dignissimos, perferendis eaque ut sed magni deserunt id.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo. Saepe fuga rerum laudantium tempora minima sapiente minus nemo voluptatum nihil molestias commodi temporibus blanditiis, fugit libero? Doloremque molestias ea voluptatibus maxime ipsam corporis illum, neque, modi cumque ipsum reiciendis asperiores distinctio dignissimos, perferendis eaque ut sed magni deserunt id.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo. Saepe fuga rerum laudantium tempora minima sapiente minus nemo voluptatum nihil molestias commodi temporibus blanditiis, fugit libero? Doloremque molestias ea voluptatibus maxime ipsam corporis illum, neque, modi cumque ipsum reiciendis asperiores distinctio dignissimos, perferendis eaque ut sed magni deserunt id.</p>
         <h2>Content system.</h2>
         <button>Click me</button>
         </>

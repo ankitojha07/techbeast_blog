8dc3efd6b63b3cf2e1c6bfe549152253acbb1f2e
Ankit Ojha <ankitojha1409@gmail.com>
2022-12-02 02:46:02 +0530

    started working on next.tsx

--- pages/learn/next.tsx
+++ pages/learn/next.tsx
@@ -2,7 +2,7 @@ export default function LearnNext(){
     return (
         <>
         <div className="flex justify-center">
-            <h1 className="text-center mt-10">This file is for learning Next JS</h1>
+            <h1 className="text-3xl font-bold mt-10 text-blue-400">This file is for learning Next JS</h1>
         </div>
         </>
     )

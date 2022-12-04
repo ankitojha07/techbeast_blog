2fbb8775a1eff291dad6cbb929d000949dde9f96
Ankit Ojha <ankitojha1409@gmail.com>
2022-12-04 23:31:11 +0530

    hello file title change

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,7 +1,7 @@
 export default function Hello(){
     return (
         <>
-        <h1>Learn Next JS.</h1>
+        <h1>Hello from developers!</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo. Saepe fuga rerum laudantium tempora minima sapiente minus nemo voluptatum nihil molestias commodi temporibus blanditiis, fugit libero? Doloremque molestias ea voluptatibus maxime ipsam corporis illum, neque, modi cumque ipsum reiciendis asperiores distinctio dignissimos, perferendis eaque ut sed magni deserunt id.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo. Saepe fuga rerum laudantium tempora minima sapiente minus nemo voluptatum nihil molestias commodi temporibus blanditiis, fugit libero? Doloremque molestias ea voluptatibus maxime ipsam corporis illum, neque, modi cumque ipsum reiciendis asperiores distinctio dignissimos, perferendis eaque ut sed magni deserunt id.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo. Saepe fuga rerum laudantium tempora minima sapiente minus nemo voluptatum nihil molestias commodi temporibus blanditiis, fugit libero? Doloremque molestias ea voluptatibus maxime ipsam corporis illum, neque, modi cumque ipsum reiciendis asperiores distinctio dignissimos, perferendis eaque ut sed magni deserunt id.</p>

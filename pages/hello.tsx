c6137b9084072b1714ef9362e0dbbe30b9d9e391
Ankit Ojha <ankitojha1409@gmail.com>
2022-12-05 01:05:02 +0530

    learning test

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,7 +1,9 @@
+import styles from '../styles/Home.module.css'
+
 export default function Hello(){
     return (
         <>
-        <h1>Hello from developers!</h1>
+            <h1 className={styles.title}>Hello from developers!</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo. Saepe fuga rerum laudantium tempora minima sapiente minus nemo voluptatum nihil molestias commodi temporibus blanditiis, fugit libero? Doloremque molestias ea voluptatibus maxime ipsam corporis illum, neque, modi cumque ipsum reiciendis asperiores distinctio dignissimos, perferendis eaque ut sed magni deserunt id.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo. Saepe fuga rerum laudantium tempora minima sapiente minus nemo voluptatum nihil molestias commodi temporibus blanditiis, fugit libero? Doloremque molestias ea voluptatibus maxime ipsam corporis illum, neque, modi cumque ipsum reiciendis asperiores distinctio dignissimos, perferendis eaque ut sed magni deserunt id.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nemo. Saepe fuga rerum laudantium tempora minima sapiente minus nemo voluptatum nihil molestias commodi temporibus blanditiis, fugit libero? Doloremque molestias ea voluptatibus maxime ipsam corporis illum, neque, modi cumque ipsum reiciendis asperiores distinctio dignissimos, perferendis eaque ut sed magni deserunt id.</p>
---
+++ pages/learn/mouce.tsx
@@ -0,0 +1,9 @@
+export default function Mouce(){
+    return (
+        <>
+        <div className="flex justify-center">
+            <h1 className="text-3xl font-bold mt-10 text-blue-400">Mouce Page</h1>
+        </div>
+        </>
+    )
+}

9df620425b66afcfed73cfdc476f53da7bb7e212
Ankit Ojha <ankitojha1409@gmail.com>
2022-12-08 10:06:04 +0530

    pages and logo removed

--- pages/learn/mouce.tsx
+++
@@ -1,9 +0,0 @@
-export default function Mouce(){
-    return (
-        <>
-        <div className="flex justify-center">
-            <h1 className="text-3xl font-bold mt-10 text-blue-400">Mouce Page</h1>
-        </div>
-        </>
-    )
-}
